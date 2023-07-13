8b07efb9219235da801c811aa3bdb091ebcb63bf
hyeonggyu <hyeonggyu.Kang@hyperlounge.ai>
2023-07-13 21:37:05 +0900

    CodeBehind 실습과 자동으로 ComponentBase를 상속받는것을 확인

---
+++ Blazor_Academy/Pages/About.cs
@@ -0,0 +1,13 @@
+﻿namespace Blazor_Academy.Pages
+{
+    public partial class About
+    {
+        string title = "정보";
+        string subTitle = "서브 타이틀";
+
+        protected override void OnInitialized()
+        {
+            subTitle = System.DateTime.Now.ToLongTimeString();
+        }
+    }
+}
